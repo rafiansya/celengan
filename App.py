1e4583dd2d02b7e1365dce0a6331f3738d8b68aa
rafiansya <115703789+rafiansya@users.noreply.github.com>
2025-05-22 08:44:49 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.image("https://media.tenor.com/3tXTApJ-BQ0AAAAC/naruto-running.gif")
+st.title("STD!")
+st.write("MAIN SG BENER SU")
+st.image ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTZCaFSeWstJqL-rzPMk7rnZKExPymU7lSiA&s", caption=" JAWA!", use_column_width=True)
